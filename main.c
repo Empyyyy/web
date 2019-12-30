f9424f710d31f9334f586333f8d40993bbaa35e4
Empyyyy <616742875@qq.com>
2019-12-30 20:57:09 +0800

    添加了错误处理函数

--- main.c
+++ main.c
@@ -6,21 +6,18 @@ int main(int argc,char **argv)
     int server_sockert,client_socket,clientlen;
 	struct sockaddr_in client_addr;
 	rio_t rio;
-	port = 1024 ;
-	
+	int port = 1024 ;
+	char buf[1024],method[1024],uri[1024],verison[1024];
 	server_sockert=open_listen_sock(port);
 	
 	while(1)
 	{
 		clientlen = sizeof(client_addr);
 		client_socket = accept(server_sockert,(struct sockaddr*)&client_addr,&clientlen);
-		
+		if(client_socket < 0)
+			perror("accept");
+
 		close(client_socket);
 	}
 }
 
-void accept_request(int client)
-{
-     int flag ;//判断是动态还是静态
-     	 
-}
